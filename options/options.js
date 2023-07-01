ecdad92276cf769673db3347a87ae58d599c67f1
Crowdbotics <team@crowdbotics.com>
2023-07-01 23:31:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "barkpage-41850",
+  url: "https://barkpage-41850.botics.co",
+  api: "https://barkpage-41850.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
